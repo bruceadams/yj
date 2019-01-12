9b90f46325c7ef2b5be9e5241bd403a8176e8591
Bruce Adams <bruce.adams@acm.org>
2019-01-12 12:36:39 -0500

    Travis builds to Github release

---
+++ release-build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+BINARY=yj
+OS_NAME="$TRAVIS_OS_NAME"
+
+if [[ "$OS_NAME" == "linux" ]]; then
+    # Special handling for Linux: build a static binary
+    export CC=clang
+    export CXX=clang++
+    rustup component add x86_64-unknown-linux-musl
+    cargo build --release --target x86_64-unknown-linux-musl
+    mv "target/x86_64-unknown-linux-musl/release/$BINARY" "$BINARY.$OS_NAME"
+else
+    cargo build --release
+    mv "target/release/$BINARY" "$BINARY.$OS_NAME"
+fi
