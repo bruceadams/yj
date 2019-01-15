d44a331134cbea06946856e7628a644597f97fc3
Bruce Adams <bruce.adams@acm.org>
2019-01-14 21:36:08 -0500

    Publish crate from Linux only

--- release-build.sh
+++ release-build.sh
@@ -3,16 +3,21 @@
 set -euxo pipefail
 
 BINARY=yj
-OS_NAME="$TRAVIS_OS_NAME"
 
-if [[ "$OS_NAME" == "linux" ]]; then
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     # Special handling for Linux: build a static binary
     export CC=clang
     export CXX=clang++
     rustup target add x86_64-unknown-linux-musl
     cargo build --release --target x86_64-unknown-linux-musl
-    mv "target/x86_64-unknown-linux-musl/release/$BINARY" "$BINARY.$OS_NAME"
+    mv -f "target/x86_64-unknown-linux-musl/release/$BINARY" "target/$BINARY.linux"
+elif [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+    cargo build --release
+    mv -f "target/release/$BINARY" "target/$BINARY.macos"
+elif [[ "$TRAVIS_OS_NAME" == "windows" ]]; then
+    cargo build --release
+    mv -f "target/release/$BINARY.exe" "target"
 else
     cargo build --release
-    mv "target/release/$BINARY" "$BINARY.$OS_NAME"
+    mv -f "target/release/$BINARY" "target/$BINARY.$TRAVIS_OS_NAME"
 fi
